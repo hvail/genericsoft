0f621608118ed55088283d3823578d35c5313bee
hvail <415195993@qq.com>
2016-12-31 23:08:54 +0800

    v2.4.1

--- startrabbit.sh
+++ startrabbit.sh
@@ -17,6 +17,7 @@ change_default_user() {
 		rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS
 		rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
 		rabbitmqctl set_permissions -p / $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+		echo "End adding ${RABBITMQ_DEFAULT_USER}"
 	fi
 }
 
