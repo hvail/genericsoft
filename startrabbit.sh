c58e6367996276664dc73cdca062b7c4be74e7be
hvail <415195993@qq.com>
2017-01-01 22:01:29 +0800

    v2.7.0

--- startrabbit.sh
+++ startrabbit.sh
@@ -30,44 +30,30 @@ ls -al /var/lib/rabbitmq/
 
 if [ -z "$CLUSTERED" ]; then
 	# if not clustered then start it normally as if it is a single server
-	echo 'Start Rabbitmq Server'
-	# rabbitmq-server start
-	# rabbitmqctl status
-	# rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
-	# tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
+	/usr/sbin/rabbitmq-server &
+	rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 	change_default_user	
-	rabbitmq-server
+	tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 else
-	echo 'Start Rabbitmq CLUSTER'
 	if [ -z "$CLUSTER_WITH" ]; then
-		echo 'CLUSTER_WITH EXISTS'
 		# If clustered, but cluster with is not specified then again start normally, could be the first server in the
 		# cluster
-		# /usr/sbin/rabbitmq-server&
-		# rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
-		# tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
-		rabbitmq-server
+		/usr/sbin/rabbitmq-server&
+		rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	else
-		echo 'NOT CLUSTER_WITH EXISTS'
-		# /usr/sbin/rabbitmq-server & rabbitmqctl wait
 		/usr/sbin/rabbitmq-server &
 		rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
-		rabbitmq-server -detached
-		# rabbitmqctl cluster_status
 		rabbitmqctl stop_app
-		rabbitmqctl reset
 		if [ -z "$RAM_NODE" ]; then
-			echo 'NOT RAM MODE'
 			rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
 		else
-			echo 'RAM MODE'
 			rabbitmqctl join_cluster --ram rabbit@$CLUSTER_WITH
 		fi
 		rabbitmqctl start_app
-		rabbitmq-server
                 
 		# Tail to keep the a foreground process active..
-		# tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
+		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	fi
 fi
 
